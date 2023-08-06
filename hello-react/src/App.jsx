66f6521d0d54484fc80485bb84c1222f8f7f5dd9
Sridhar-89 <sridharbodhanapu89@gmail.com>
2023-08-06 22:41:04 +0530

    modified style

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -12,17 +12,17 @@ function App() {
     
 
 <div>
-      <div className="bg-white-500 p-4">
-        <h2 className="text-black text-2xl font-bold mb-2">Smarter Tasks</h2>
+      <div className="bg-white-500 p-4 text-left">
+        <h2 className="text-black text-3xl font-bold mb-2">Smarter Tasks</h2>
         <p className="text-black">
           <strong>Project:</strong> <span>Graduation Final Year project (Revamp College Website)</span>
         </p>
       </div>
 
-      <div className="flex">
-        <div className="w-1/2 border border-white-10 rounded-xl p-8 mr-4">
-        <h2 className="text-black text-xl font-bold mb-2">Pending</h2>
-        <TaskCard title="Buid the website with static content" dueDate="10th April" assigneeName="Rohit S"/>
+      <div className="flex px-2.5">
+        <div className="w-1/2 border border-white-10 rounded-2xl p-4 mr-4 text-left fill-c">
+        <h2 className="text-black text-2xl font-bold mb-2 text-center">Pending</h2>
+        <TaskCard title="Build the website with static content" dueDate="10th April" assigneeName="Rohit S"/>
         <TaskCard title="Add a Blog" dueDate="22nd March" assigneeName="Rohit M"/> 
         
         <div className="button">
@@ -34,8 +34,8 @@ function App() {
         </div>
         
         </div>
-        <div className="w-1/2 border border-white-10 rounded-xl p-8 ml-4">
-          <h2 className="text-black text-xl font-bold mb-2">Done</h2>
+        <div className="w-1/2 border border-white-10 rounded-2xl p-4 ml-4 text-left">
+          <h2 className="text-black text-2xl font-bold mb-2 text-center">Done</h2>
           <TaskCard title="Design the mockup" completedAtDate ="10th April" assigneeName="Rohit M"/>
         <TaskCard title="Get the approval from principal" completedAtDate="20th April" assigneeName="Ajay S"/>
          
--- hello-react/src/TaskCard.jsx
+++ hello-react/src/TaskCard.jsx
@@ -4,16 +4,16 @@ const TaskCard = (props) => {
   console.log(props)
   let check;
   if(props.dueDate){
-    check="Due on :" +props.dueDate
+    check="Due on:" + props.dueDate
   }
   else{
-    check="Completed on :" +props.completedAtDate
+    check="Completed on:" + props.completedAtDate
   }
   return (
     <div className='TaskItem'>
-      <h2 className="text-xl font-bold">{props.title}</h2>
-      <p>{check}</p>
-      <p>Assignee : {props.assigneeName}</p>
+      <h2 className="text-xl font-bold px-2 py-2">{props.title}</h2>
+      <p className="px-2">{check}</p>
+      <p className="px-2">Assignee: {props.assigneeName}</p>
     </div>
   )
 }
