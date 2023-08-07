4d5b39b3f5a9570b97045f2ab17a37d85804b04e
Sridhar-89 <sridharbodhanapu89@gmail.com>
2023-08-07 23:02:02 +0530

    changed the logic of passing

--- hello-react/src/TaskCard.jsx
+++ hello-react/src/TaskCard.jsx
@@ -1,16 +1,15 @@
 import './TaskCard.css'
 
 const TaskCard = (props) => {
-  console.log(props)
-  let check;
+  
   if(props.dueDate){
-    check=props.dueDate
+
     return (
       <>
 
       <div className='TaskItem'>
         <h2 className="text-xl font-bold px-2 py-2">{props.title}</h2>
-        <p className="px-2">Due on: {check}</p>
+        <p className="px-2">Due on: {props.dueDate}</p>
         <p className="px-2">Assignee: {props.assigneeName}</p>
       </div>
       </>
@@ -18,13 +17,13 @@ const TaskCard = (props) => {
     
   }
   else{
-    check=props.completedAtDate
+
   return (
     <>
     
     <div className='TaskItem'>
       <h2 className="text-xl font-bold px-2 py-2">{props.title}</h2>
-      <p className="px-2">Completed on: {check}</p>
+      <p className="px-2">Completed on: {props.completedAtDate}</p>
       <p className="px-2">Assignee: {props.assigneeName}</p>
     </div>
     </>
