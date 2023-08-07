361849cfc81707a414abe1c906a95e1e4425ab5f
Sridhar-89 <sridharbodhanapu89@gmail.com>
2023-08-07 22:39:19 +0530

    add react return component in TaskCard.jsx

--- hello-react/src/TaskCard.jsx
+++ hello-react/src/TaskCard.jsx
@@ -6,24 +6,28 @@ const TaskCard = (props) => {
   if(props.dueDate){
     check=props.dueDate
     return (
+      <>
 
       <div className='TaskItem'>
         <h2 className="text-xl font-bold px-2 py-2">{props.title}</h2>
         <p className="px-2">Due on: {check}</p>
         <p className="px-2">Assignee: {props.assigneeName}</p>
       </div>
+      </>
     )
     
   }
   else{
     check=props.completedAtDate
   return (
+    <>
     
     <div className='TaskItem'>
       <h2 className="text-xl font-bold px-2 py-2">{props.title}</h2>
       <p className="px-2">Completed on: {check}</p>
       <p className="px-2">Assignee: {props.assigneeName}</p>
     </div>
+    </>
   )
   }
 }
