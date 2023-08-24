08ac82979e1746d09c73b9454d5a99ea40a91114
Sridhar-89 <sridharbodhanapu89@gmail.com>
2023-08-24 14:54:30 +0530

    modified main.tsx

--- smarter-tasks/src/main.tsx
+++ smarter-tasks/src/main.tsx
@@ -1,16 +1,16 @@
-// import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-   <App />
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+// root.render(
+//    <App />
+// );
