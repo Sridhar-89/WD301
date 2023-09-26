033a506cef535fb9dfe0240d74800f53812405ee
Sridhar-89 <sridharbodhanapu89@gmail.com>
2023-09-26 16:44:31 +0530

    changed the path of Notfound.tsx

--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -7,7 +7,7 @@ import Signup from "../pages/signup";
 import Projects from "../pages/projects";
 import Members from "../pages/members";
 import Logout from "../pages/logout";
-import Notfound from "../Notfound";
+import Notfound from "../pages/Notfound";
 
 const router = createBrowserRouter([
   { path: "/", element: <Navigate to="/account/projects" replace /> },
